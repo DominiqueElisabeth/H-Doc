4b0ecde0e12d62004185c6bb5e051c523187e9b7
dominique owoua <domelisa@ymail.com>
2021-09-07 05:46:29 +0000

    config devise and local

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module MedicalPassApp2
     config.load_defaults 5.2
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+  
 
     config.exceptions_app = ->(env) { ErrorsController.action(:show).call(env) }
 
