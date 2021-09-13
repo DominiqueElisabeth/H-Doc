09ab7f5c5487db001190ea42bb27587f9a4ebc48
dominique owoua <domelisa@ymail.com>
2021-09-13 13:05:41 +0000

    posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,5 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
-  before_action :set_patient
 
   # GET /posts or /posts.json
   def index
